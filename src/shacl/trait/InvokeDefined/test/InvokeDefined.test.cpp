7bf8a53cf1970bd5cb1883f2ab59a53b4816adc9
Austin McCartney <amccartney@lanl.gov>
2018-08-22 06:04:39 -0600

    more SFINAE on INVOKE and expanded InvokeDefined testing

--- src/shacl/trait/InvokeDefined/test/InvokeDefined.test.cpp
+++ src/shacl/trait/InvokeDefined/test/InvokeDefined.test.cpp
@@ -5,16 +5,189 @@
 #include <string>
 
 struct Fn {
-  void operator()(int){}
+  void operator()(int) const {}
+  virtual void call(int) const {}
+  int field;
+  virtual ~Fn() = default;
 };
 
+struct Child : Fn {
+  void call(int) const override {}
+};
+
+struct Gn {
+  void call(int) const {}
+};
+
+void fn(int){}
+
 SCENARIO("InvokeDefined"){
-  GIVEN("a callable and a compatible argument"){
-    constexpr bool invocable = shacl::trait::InvokeDefined_v<Fn, int>;
-    REQUIRE(invocable);
+  GIVEN("a free function"){
+    GIVEN("a compatible argument"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v<decltype(fn), int>;
+      REQUIRE(invocable);
+    }
+    GIVEN("an incompatible argument"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v<decltype(fn), std::string>;
+      REQUIRE(not invocable);
+    }
   }
-  GIVEN("a callable and a incompatible argument"){
-    constexpr bool invocable = shacl::trait::InvokeDefined_v<Fn, std::string>;
-    REQUIRE(not invocable);
+  GIVEN("a callable struct"){
+    GIVEN("a compatible argument"){
+      constexpr bool invocable = shacl::trait::InvokeDefined_v<Fn&, int>;
+      REQUIRE(invocable);
+    }
+    GIVEN("an incompatible argument"){
+      constexpr bool invocable = shacl::trait::InvokeDefined_v<Fn&, std::string>;
+      REQUIRE(not invocable);
+    }
+  }
+  GIVEN("a member function pointer"){
+    auto call = &Fn::call;
+    GIVEN("a class reference"){
+      GIVEN("a compatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v<decltype(call), Fn&, int>;
+        REQUIRE(invocable);
+      }
+      GIVEN("an incompatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v<decltype(call), Fn&, std::string>;
+        REQUIRE(not invocable);
+      }
+    }
+    GIVEN("a class pointer"){
+      GIVEN("a compatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v<decltype(call), Fn*, int>;
+        REQUIRE(invocable);
+      }
+      GIVEN("an incompatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v<decltype(call), Fn*, std::string>;
+        REQUIRE(not invocable);
+      }
+    }
+    GIVEN("a class reference_wrapper"){
+      GIVEN("a compatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v
+          <decltype(call), std::reference_wrapper<Fn>, int>;
+        REQUIRE(invocable);
+      }
+      GIVEN("an incompatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v
+          <decltype(call), std::reference_wrapper<Fn>, std::string>;
+        REQUIRE(not invocable);
+      }
+    }
+    GIVEN("a child class reference"){
+      GIVEN("a compatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v<decltype(call), Child&, int>;
+        REQUIRE(invocable);
+      }
+      GIVEN("an incompatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v<decltype(call), Child&, std::string>;
+        REQUIRE(not invocable);
+      }
+    }
+    GIVEN("a child class pointer"){
+      GIVEN("a compatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v<decltype(call), Child*, int>;
+        REQUIRE(invocable);
+      }
+      GIVEN("an incompatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v<decltype(call), Child*, std::string>;
+        REQUIRE(not invocable);
+      }
+    }
+    GIVEN("a child class reference_wrapper"){
+      GIVEN("a compatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v
+          <decltype(call), std::reference_wrapper<Child>, int>;
+        REQUIRE(invocable);
+      }
+      GIVEN("an incompatible argument"){
+        constexpr bool invocable =
+          shacl::trait::InvokeDefined_v
+          <decltype(call), std::reference_wrapper<Child>, std::string>;
+        REQUIRE(not invocable);
+      }
+    }
+    GIVEN("an unrelated class reference"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v<decltype(call), Gn&, int>;
+      REQUIRE(not invocable);
+    }
+    GIVEN("an unrelated class pointer"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v<decltype(call), Gn*, int>;
+      REQUIRE(not invocable);
+    }
+    GIVEN("an unrelated class reference_wrapper"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v
+        <decltype(call), std::reference_wrapper<Gn>, int>;
+      REQUIRE(not invocable);
+    }
+  }
+  GIVEN("a member object pointer"){
+    auto field = &Fn::field;
+    GIVEN("a class reference"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v<decltype(field), Fn&>;
+      REQUIRE(invocable);
+    }
+    GIVEN("a class pointer"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v<decltype(field), Fn*>;
+      REQUIRE(invocable);
+    }
+    GIVEN("a class reference_wrapper"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v
+        <decltype(field), std::reference_wrapper<Fn>>;
+      REQUIRE(invocable);
+    }
+    GIVEN("a child class reference"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v<decltype(field), Child&>;
+      REQUIRE(invocable);
+    }
+    GIVEN("a child class pointer"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v<decltype(field), Child*>;
+      REQUIRE(invocable);
+    }
+    GIVEN("a child class reference_wrapper"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v
+        <decltype(field), std::reference_wrapper<Child>>;
+      REQUIRE(invocable);
+    }
+    GIVEN("an unrelated class reference"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v<decltype(field), Gn&>;
+      REQUIRE(not invocable);
+    }
+    GIVEN("an unrelated class pointer"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v<decltype(field), Gn*>;
+      REQUIRE(not invocable);
+    }
+    GIVEN("an unrelated class reference_wrapper"){
+      constexpr bool invocable =
+        shacl::trait::InvokeDefined_v
+        <decltype(field), std::reference_wrapper<Gn>>;
+      REQUIRE(not invocable);
+    }
   }
 }
