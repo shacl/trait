cbdd3b60349510d06132b2b8c6a152d4525fc036
Austin Paul McCartney <amccartney@lanl.gov>
2018-10-05 13:06:53 -0600

    Addressed bug in void_t with C++17

--- src/shacl/trait/void_t.hpp
+++ src/shacl/trait/void_t.hpp
@@ -2,13 +2,14 @@
   namespace detail {
 
   template<typename... Ts>
-  struct make_void { using type = void;};
+  struct make_void { using type = void; };
 
   }
 
   template<typename... Ts>
   using void_t = typename detail::make_void<Ts...>::type;
+
 #else
   template<typename... Ts>
-  using void_t = std::void_t<Ts...>
+  using void_t = std::void_t<Ts...>;
 #endif
