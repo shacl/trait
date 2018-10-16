738c02910ca01a7e463dcec538753d67b7b1c85c
David A. Dixon <ddixon@lanl.gov>
2018-10-16 14:14:31 -0600

    typo in HasType test and added protected inheritance type test

--- src/shacl/trait/HasType/test/HasType.test.cpp
+++ src/shacl/trait/HasType/test/HasType.test.cpp
@@ -6,20 +6,29 @@ struct Does {
   using type = int;
 };
 
+struct Protected : protected Does{};
+
 struct Doesnt {};
 
 SCENARIO("HasType"){
-  GIVEN("a type with a defined nested 'test'"){
+  GIVEN("a type with a defined nested 'type'"){
     THEN("the nested value is true"){
       constexpr bool hasType = shacl::trait::HasType_v<Does>;
       REQUIRE(hasType);
     }
   }
 
-  GIVEN("a type without a defined nested 'test'"){
+  GIVEN("a type without a defined nested 'type'"){
     THEN("the nested value is false"){
       constexpr bool hasType = shacl::trait::HasType_v<Doesnt>;
       REQUIRE(not hasType);
     }
   }
+
+  GIVEN("a type with a defined protected nested 'type'"){
+    THEN("the nested value is false"){
+      constexpr bool hasType = shacl::trait::HasType_v<Protected>;
+      REQUIRE(not hasType);
+    }
+  }  
 }
