b60fae8d804a08f007553e2ddc17d3e63a5fd1e2
Austin McCartney <amccartney@lanl.gov>
2018-12-13 08:52:32 -0700

    Clean up DereferenceResult test

--- src/shacl/trait/DereferenceResult/test/DereferenceResult.test.cpp
+++ src/shacl/trait/DereferenceResult/test/DereferenceResult.test.cpp
@@ -10,27 +10,55 @@ struct NotDereferenceable {};
 
 SCENARIO("DereferenceResult"){
   GIVEN("a dereferenceable type"){
-    using result_t = shacl::trait::Dereferenced_t<Dereferenceable>;
-    constexpr bool affirmative = std::is_same<bool, result_t>::value;
-    REQUIRE(affirmative);
-  }
+    THEN("the Dereferenced trait will have a nested type alias"){
+      constexpr bool affirmative =
+        shacl::trait::HasType_v<shacl::trait::Dereferenced<Dereferenceable>>;
+      REQUIRE(affirmative);
+    }
 
-  GIVEN("a dereferenceable type"){
-    using result_t = shacl::trait::DereferenceResult_t<Dereferenceable>;
-    constexpr bool affirmative = std::is_same<bool, result_t>::value;
-    REQUIRE(affirmative);
+    THEN("the Derefenced trait nested alias will refer to the correct type"){
+      REQUIRE(std::is_same
+              <shacl::trait::Dereferenced<Dereferenceable>::type, bool>::value);
+    }
+
+    THEN("the Dereferenced_t alias will refer to the correct type"){
+      using result_t = shacl::trait::Dereferenced_t<Dereferenceable>;
+      constexpr bool affirmative = std::is_same<bool, result_t>::value;
+      REQUIRE(affirmative);
+    }
+
+    THEN("the DereferenceResult trait will have a nested type alias"){
+      constexpr bool affirmative =
+        shacl::trait::HasType_v
+        <shacl::trait::DereferenceResult<Dereferenceable>>;
+      REQUIRE(affirmative);
+    }
+
+    THEN("the Derefenced trait nested alias will refer to the correct type"){
+      REQUIRE(std::is_same
+              <shacl::trait::DereferenceResult
+               <Dereferenceable>::type, bool>::value);
+    }
+
+    THEN("the DereferenceResult_t alias will refer to the correct type"){
+      using result_t = shacl::trait::DereferenceResult_t<Dereferenceable>;
+      constexpr bool affirmative = std::is_same<bool, result_t>::value;
+      REQUIRE(affirmative);
+    }
   }
 
   GIVEN("a non-dereferenceable type"){
-    constexpr bool negative =
-      shacl::trait::HasType_v<shacl::trait::Dereferenced<NotDereferenceable>>;
-    REQUIRE_FALSE(negative);
-  }
+    THEN("the Dereferenced trait will NOT have a nested type alias"){
+      constexpr bool negative =
+        shacl::trait::HasType_v<shacl::trait::Dereferenced<NotDereferenceable>>;
+      REQUIRE_FALSE(negative);
+    }
 
-  GIVEN("a non-dereferenceable type"){
-    constexpr bool negative =
-      shacl::trait::HasType_v
-      <shacl::trait::DereferenceResult<NotDereferenceable>>;
-    REQUIRE_FALSE(negative);
+    THEN("the DereferenceResult trait will NOT have a nested type alias"){
+      constexpr bool negative =
+        shacl::trait::HasType_v
+        <shacl::trait::DereferenceResult<NotDereferenceable>>;
+      REQUIRE_FALSE(negative);
+    }
   }
 }
