dad2eca42255f4e358a25f2b7f54e9013e7b7377
Austin McCartney <amccartney@lanl.gov>
2018-12-13 10:08:08 -0700

    Removed C++17 valuas alias

--- src/shacl/trait/getterResult.hpp
+++ src/shacl/trait/getterResult.hpp
@@ -5,8 +5,8 @@ struct getterResult_fn {
     <std::is_lvalue_reference<T>::value
      and std::is_const<std::remove_reference_t<T>>::value,
      std::conditional_t
-     <std::is_trivially_copyable_v<std::decay_t<T>>
-      and std::is_trivially_destructible_v<std::decay_t<T>>
+     <std::is_trivially_copyable<std::decay_t<T>::value>
+      and std::is_trivially_destructible<std::decay_t<T>::value>
       and (sizeof(std::decay_t<T>) < 9),
        std::decay_t<T>, const std::decay_t<T>&>,
       T>;
