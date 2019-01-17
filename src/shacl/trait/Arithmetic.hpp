36bb22f43be7d78cdfc292512166c4e5e8fc0233
Austin McCartney <amccartney@lanl.gov>
2019-01-17 14:19:28 -0700

    what monster set the requirement to 17?! whoops

--- src/shacl/trait/Arithmetic.hpp
+++ src/shacl/trait/Arithmetic.hpp
@@ -2,7 +2,8 @@ template<typename Left, typename Right = Left>
 constexpr const bool Arithmetic_v =
   AdditionDefined_v<Left, Right>
   and SubtractionDefined_v<Left, Right>
-  and Scalable_v<Left, Right>;
+  and Scalable_v<Left, Right>
+  and Comparable_v<Left, Right>;
 
 template<typename... Args>
 using Arithmetic = bool_t<Arithmetic_v<Args...>>;
